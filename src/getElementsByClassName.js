419df72667058d0ad3b983ddc8cc17b3b9713834
GrokN <FullGrok@gmail.com>
2017-04-27 02:36:09 -0700

    Implement getElementByClassName recursively

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,23 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// traverse the DOM tree recursively
+function traverseTree(className, root, elementArr) {
+
+    if(root.classList && root.classList.contains(className))
+        elementArr.push(root)
+
+    if(!root.hasChildNodes())
+        return
+
+    for(let i = 0; i < root.childNodes.length; i++)
+        traverseTree(className, root.childNodes[i], elementArr)
+}
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className ) {
+    let elementArr = []
+
+    traverseTree(className, document.body, elementArr)
+    return elementArr
 };
